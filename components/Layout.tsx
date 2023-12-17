de7db0224c888cbe85059586c6b073f43fd65ff0
Youngwook-Jeon <ela87@naver.com>
2023-12-18 00:59:31 +0900

    commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
